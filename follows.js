134c9e2bffc973a200677d74033e52c1f4cd98b5
lost dog <lost.dog@libero.it>
2014-01-05 09:06:15 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,7 +12,7 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
+	var defaultKey		= 'l8QSLWjdzFrQOVQa7ARczYWMOBvMqPwRiu1tpIOXyGxLJWWF', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Mau Home Electric Consumptions', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
