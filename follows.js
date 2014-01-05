11106b06d1229876cfb146d623916497a3574663
lost dog <lost.dog@libero.it>
2014-01-05 19:15:49 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
         var defaultKey                = 'l8QSLWjdzFrQOVQa7ARczYWMOBvMqPwRiu1tpIOXyGxLJWWF', // Unique master Xively API key to be used as a default
                 defaultFeeds        = [1163489474!0!1!10!], // Comma separated array of Xively Feed ID numbers
-                applicationName        = 'Mau', // Replaces Xively logo in the header
+                applicationName        = 'Mau home', // Replaces Xively logo in the header
                 dataDuration        = '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
                 dataInterval        = 0, // Default interval for data to be displayed (in seconds)
                 dataColor                = '', // CSS HEX value of color to represent data (omit leading #)
